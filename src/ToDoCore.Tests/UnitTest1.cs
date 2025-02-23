3d1dad7c5b72e805a1a46deaafaf6ed59e3c102a
Walter <wrstocke@googlemail.com>
2025-02-23 11:34:04 +0100

    test: add unit tests for Task

---
+++ src/ToDoCore.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoCore.Tests;
+
+public class TaskTests
+{
+    [Test]
+    public void NewlyCreatedTaskShouldNotBeCompleted()
+    {
+        var task = new Task("Test task");
+        Assert.That(task.IsCompleted, Is.False);
+    }
+}
