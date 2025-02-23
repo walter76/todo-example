0a245752002695b08d9488b4cd89aa0a6011a0c3
Walter <wrstocke@googlemail.com>
2025-02-23 11:17:37 +0100

    feat: add Task

---
+++ src/ToDoCore/Task.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoCore;
+
+public class Task
+{
+    public Guid Id { get; private set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public Task(string title)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        IsCompleted = false;
+    }
+}
